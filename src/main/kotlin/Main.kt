b79ad862e8e2ba9da71ee49401377c79c50ae16a
Dennis Hsieh <yundom@gmail.com>
2024-03-24 01:09:55 +0800

    Run CliApp

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,20 @@
 package com.github.yundom
 
 fun main() {
-    println("Hello World!")
+    val output = Components.provideCli()
+    val app = Components.provideCliApp()
+
+    while (true) {
+        print("> ")
+
+        val input = readlnOrNull() ?: continue
+
+        if (input.equals("exit", ignoreCase = true)) {
+            break
+        }
+
+        if (app.parse(input).not()) {
+            output("Unknown command: $input")
+        }
+    }
 }
