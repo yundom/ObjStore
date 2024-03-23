acefaa6c6470057604782ed91178f58b49dbe972
Dennis Hsieh <yundom@gmail.com>
2024-03-23 16:37:44 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+}
+
+group = "com.github.yundom"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(11)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package com.github.yundom
+
+fun main() {
+    println("Hello World!")
+}
